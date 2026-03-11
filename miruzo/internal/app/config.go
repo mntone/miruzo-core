8343fd51c0b1d1f71858bee5b2e3271b451aae97
mntone <901816+mntone@users.noreply.github.com>
2026-03-11 16:17:32 +0900

    feat(config): auto-generate config.yaml from embedded sample
    - embed config.sample.yaml
    - create config.yaml when missing

--- miruzo/internal/app/config.go
+++ miruzo/internal/app/config.go
@@ -1,17 +1,40 @@
 package app
 
 import (
+	_ "embed"
+	"errors"
 	"fmt"
+	"os"
 
 	"github.com/mntone/miruzo-core/miruzo/internal/config"
 	"github.com/spf13/viper"
 )
+
+//go:embed config.sample.yaml
+var sampleConfigData []byte
+
+func writeSampleConfigFile() error {
+	f, err := os.Create("config.yaml")
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	_, err = f.Write(sampleConfigData)
+	return err
+}
 
 func LoadConfig() (config.AppConfig, error) {
 	viper.SetConfigType("yaml")
 	viper.AddConfigPath(".")
 
 	if err := viper.ReadInConfig(); err != nil {
+		if _, notFoundError := errors.AsType[viper.ConfigFileNotFoundError](err); notFoundError {
+			if writeError := writeSampleConfigFile(); writeError != nil {
+				return config.AppConfig{}, fmt.Errorf("write sample config: %w", writeError)
+			}
+		}
+
 		return config.AppConfig{}, fmt.Errorf("read config: %w", err)
 	}
 
