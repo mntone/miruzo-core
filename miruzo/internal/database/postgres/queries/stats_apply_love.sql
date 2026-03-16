ecb8787153fe1a9e8c87fe71e26cb29cdbdead00
mntone <901816+mntone@users.noreply.github.com>
2026-03-17 08:02:35 +0900

    style(sql): align WHERE/AND in ApplyLoveToStats
    - adjust formatting to match existing WHERE/AND style
    - compress lines slightly for readability and consistency

--- miruzo/internal/database/postgres/queries/stats_apply_love.sql
+++ miruzo/internal/database/postgres/queries/stats_apply_love.sql
@@ -4,10 +4,8 @@ SET
 	score=score+sqlc.arg(score_delta),
 	first_loved_at=COALESCE(first_loved_at, sqlc.arg(loved_at)),
 	last_loved_at=sqlc.arg(loved_at)
-WHERE
-	ingest_id=$1
-	AND
-	(last_loved_at IS NULL OR last_loved_at < sqlc.arg(period_start_at))
+WHERE ingest_id=$1
+  AND (last_loved_at IS NULL OR last_loved_at < sqlc.arg(period_start_at))
 RETURNING score, first_loved_at, last_loved_at;
 
 -- name: ApplyLoveCanceledToStats :one
